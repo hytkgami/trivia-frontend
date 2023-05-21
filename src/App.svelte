a9fd8e98f868d84b4532137f162e59a6ee01ee20
hytkgami <gagagatrump@gmail.com>
2023-05-21 18:09:29 +0900

    add top page prototype

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,48 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import GoogleLoginButton from './GoogleLoginButton.svelte';
+  import Layout from './Layout.svelte';
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+<Layout>
+  <div slot="main" class="shrink-0 grow flex flex-col">
+    <div class="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
+      <div
+        class="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl"
+        aria-hidden="true"
+      >
+        <div
+          class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#6ee7b7] to-[#6366f1] opacity-20"
+          style="clip-path: polygon(70.9% 52.8%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 70.9% 52.8%)"
+        />
+      </div>
+      <div
+        class="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu"
+        aria-hidden="true"
+      >
+        <div
+          class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#e11d48] to-[#6366f1] opacity-40"
+          style="clip-path: polygon(70.9% 52.8%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 70.9% 52.8%)"
+        />
+      </div>
+      <div class="mx-auto max-w-7xl px-6 lg:px-8">
+        <div class="mx-auto max-w-2xl lg:mx-0">
+          <h2 class="text-4xl font-bold tracking-normal text-white sm:text-6xl">
+            <span class="tracking-wide">Trivia</span> へようこそ
+          </h2>
+          <p class="mt-6 text-lg leading-8 text-gray-300 dark:text-gray-200">
+            Triviaは、あなたの知識を試すクイズアプリです。<br />
+            ロビーに参加して、クイズを楽しみましょう！<br />
+            または、ロビーを作成して出題者になることもできます。<br />
+            あなたの出題したクイズを他の人に解いてもらいましょう！
+          </p>
+        </div>
+      </div>
+    </div>
+    <div
+      class="flex flex-col py-24 sm:py-32 bg-white shadow dark:bg-gray-800 items-center gap-2 grow text-gray-600 dark:text-gray-300"
+    >
+      <p class="text-lg leading-8">利用するにはサインインが必要です。</p>
+      <GoogleLoginButton />
+    </div>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
+</Layout>
---
+++ src/GoogleLoginButton.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import googleIcon from './assets/btn_google_light_normal_ios.svg';
+</script>
+
+<a
+  href="#"
+  class="rounded-full flex items-center gap-1 border-2 dark:border-cyan-800 bg-white dark:bg-[#4285F4] hover:bg-gray-100 dark:hover:bg-blue-700 text-gray-600 dark:text-gray-200"
+>
+  <img src={googleIcon} alt="Google" class="w-8 h-8 inline" />
+  <span class="pr-3">Sign in with Google</span></a
+>
