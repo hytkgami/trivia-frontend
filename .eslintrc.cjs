3a2da84d061d9815a4e685b41d25d6e178367d0c
hytkgami <gagagatrump@gmail.com>
2023-06-09 00:47:41 +0900

    breaking: replace svelte to react

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,14 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier"
-    ],
-    "overrides": [
-        { files: ["*.svelte"], processor: "svelte3/svelte3" }
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint",
-        "svelte3"
-    ],
-    "rules": {
-    }
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+  },
 }
--- src/lib/stores/user.ts
+++
@@ -1,4 +0,0 @@
-import type { User } from "firebase/auth";
-import { writable, type Writable } from "svelte/store";
-
-export const user: Writable<User> = writable();
